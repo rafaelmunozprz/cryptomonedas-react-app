2d71ae9417d09459254d47e7b16a04d0dd01f90b
CRELO-RAFAEL <rafaelmunoz.prz@gmail.com>
2020-03-18 13:00:28 -0600

    Actualizacion de componentes de la cotización

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -2,14 +2,21 @@ import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {CotizacionApp} from '../styled/StdApp';
 
+/**
+ * 
+ * @param {Cotizacion} resultado resultado de la consulta al API consultando la MONEDA y CRYPTOMONEDA
+ */
 const Cotizacion = ({resultado}) => {
     if(Object.keys(resultado).length === 0) return null;
-    console.log(resultado);
     return (
         <Fragment>
             <div>
                 <CotizacionApp>
-                    El precio es:  <span>{resultado.PRICE}</span>
+                    <p>El precio es:  <span>{resultado.PRICE}</span></p>
+                    <p>Precio más alto del dia:  <span>{resultado.HIGHDAY}</span></p>
+                    <p>Precio más bajo del día:  <span>{resultado.LOWDAY}</span></p>
+                    <p>Variación últimas 24 horas:  <span>{resultado.CHAGEPCT24HOUR}</span></p>
+                    <p>Última actualización:  <span>{resultado.LASTUPDATE}</span></p>
                 </CotizacionApp>
             </div>
         </Fragment>
